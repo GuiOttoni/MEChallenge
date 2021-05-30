4994f945f78cfb2eda661466bea416ebe2525fe9
Guilherme Ottoni Escarelli De Alencar <guiottonie@gmail.com>
2021-05-30 15:29:15 -0300

    adiciona models

---
+++ Pedido.Domain/Model/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEChallenge.Pedido.Domain.Model
+{
+    public class Item
+    {
+        public string Descricao { get; set; }
+        public string PrecoUnitario { get; set; }
+        public int Qtd { get; set; }
+    }
+}
---
+++ Pedido.Domain/Model/Pedido.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEChallenge.Pedido.Domain.Model
+{
+    public class Pedido
+    {
+        public string IdPedido { get; set; }
+        public IEnumerable<Item> Itens { get; set; }
+    }
+}
