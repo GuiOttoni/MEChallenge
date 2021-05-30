d2c36e8f3ff1cf8a5c9feec04dcbf23a2be1c641
Guilherme Ottoni Escarelli De Alencar <guiottonie@gmail.com>
2021-05-30 15:32:28 -0300

    adiciona projeto core e base repository

---
+++ MEChallenge.Core/BaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEChallenge.Core
+{
+    public class BaseRepository
+    {
+        internal IConfiguration _config;
+
+        internal readonly string _connectionString;
+        public BaseRepository(IConfiguration configuration)
+        {
+            _config = configuration;
+            _connectionString = _config.GetConnectionString("");
+        }
+    }
+}
